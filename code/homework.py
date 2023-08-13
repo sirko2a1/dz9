fa109b1ee23119526ba5f3b7d65ef0097464b93f
sirko2a1 <134325906+sirko2a1@users.noreply.github.com>
2023-08-13 18:20:30 +0300

    commit2

--- code/homework.py
+++ code/homework.py
@@ -23,38 +23,38 @@ class ContactBook:
         self.save_contacts()
 
     def find_contact(self, name):
-        return self.contacts.get(name, "Enter user name.")
+        return self.contacts.get(name, "Введіть username.")
 
     def update_contact(self, name, phone):
         if name in self.contacts:
             self.contacts[name] = phone
             self.save_contacts()
         else:
-            return "Enter user name."
+            return "Введіть username."
 
     def show_contacts(self):
         if self.contacts:
-            output = "Your contact book:\n"
+            output = "Ваша контактна книга:\n"
             for name, phone in self.contacts.items():
                 output += f"{name} - {phone}\n"
             return output
         else:
-            return "Your contact book is empty."
+            return "Ваша контактна книга пуста."
 
 def main():
     contact_book = ContactBook()
 
     while True:
-        command = input("Enter a command: ").lower()
+        command = input("Введіть команду: ").lower()
         words = command.split()
         command_name = words[0]
 
         if command_name == "hello":
-            print("How can I help you?")
+            print("Чим я можу допомогти?")
         elif command_name == "add":
             name, phone = words[1], words[2]
             contact_book.add_contact(name, phone)
-            print(f"Contact {name} with phone {phone} added.")
+            print(f"Контакт {name} з номером {phone} додано.")
         elif command_name == "phone":
             name = words[1]
             print(contact_book.find_contact(name))
@@ -64,14 +64,14 @@ def main():
             if result:
                 print(result)
             else:
-                print(f"Contact {name} updated with phone {phone}.")
+                print(f"Контакт {name} змінено на {phone}.")
         elif command_name == "show":
             print(contact_book.show_contacts())
         elif command_name in ("good bye", "close", "exit"):
-            print("Good bye!")
+            print("Бувайте здорові!")
             break
         else:
-            print("Unknown command. Please try again.")
+            print("Щось не так. Спробуйте знов.")
 
 if __name__ == "__main__":
     main()

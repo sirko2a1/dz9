6736adba1bdb88dc572093db5b1fcc0d18a63de0
sirko2a1 <134325906+sirko2a1@users.noreply.github.com>
2023-08-14 20:33:14 +0300

    commit3

--- code/homework.py
+++ code/homework.py
@@ -1,78 +1,88 @@
-import json
+import csv
 
-CONTACTS_FILE = "contacts.json"
+def check_file():
+    try:
+        with open('data.txt') as f:
+            DATA = list(csv.reader(f))
+    except FileNotFoundError:
+        with open('data.txt', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['name', 'age', 'phone'])
+            DATA = []
+    return DATA
 
-def error_handler(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except FileNotFoundError:
-            return {}
-    return wrapper
+def add():
+    DATA = check_file()
+    name = input('Enter name: ')
+    age = input('Enter age: ')
+    phone = input('Enter phone: ')
+    DATA.append([name, age, phone])
+    with open('data.txt', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(DATA)
+    print('Данні додані!')
 
-@error_handler
-def load_contacts():
-    with open(CONTACTS_FILE, "r") as f:
-        return json.load(f)
+def change():
+    DATA = check_file()
+    index = int(input('Введіть індекс: '))
+    name = input('Введіть нове імя: ')
+    age = input('Введіть новий вік: ')
+    phone = input('Введіть номер: ')
+    DATA[index] = [name, age, phone]
+    with open('data.txt', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(DATA)
+    print('Данні були змінені!')
 
-def save_contacts(contacts):
-    with open(CONTACTS_FILE, "w") as f:
-        json.dump(contacts, f)
-
-def add_contact(contacts, name, phone):
-    contacts[name] = phone
-    save_contacts(contacts)
-
-def find_contact(contacts, name):
-    return contacts.get(name, "Введіть username.")
-
-def update_contact(contacts, name, phone):
-    if name in contacts:
-        contacts[name] = phone
-        save_contacts(contacts)
-    else:
-        return "Введіть username."
-
-def show_contacts(contacts):
-    if contacts:
-        output = "Ваша контактна книга:\n"
-        for name, phone in contacts.items():
-            output += f"{name} - {phone}\n"
-        return output
-    else:
-        return "Ваша контактна книга пуста."
-
-def main():
-    contacts = load_contacts()
-
-    while True:
-        command = input("Введіть команду: ").lower()
-        words = command.split()
-        command_name = words[0]
-
-        if command_name == "hello":
-            print("Чим я можу допомогти?")
-        elif command_name == "add":
-            name, phone = words[1], words[2]
-            add_contact(contacts, name, phone)
-            print(f"Контакт {name} з номером {phone} додано.")
-        elif command_name == "phone":
-            name = words[1]
-            print(find_contact(contacts, name))
-        elif command_name == "change":
-            name, phone = words[1], words[2]
-            result = update_contact(contacts, name, phone)
-            if result:
-                print(result)
-            else:
-                print(f"Контакт {name} змінено на {phone}.")
-        elif command_name == "show":
-            print(show_contacts(contacts))
-        elif command_name in ("good bye", "close", "exit"):
-            print("Бувайте здорові!")
+def search():
+    DATA = check_file()
+    name = input('Введіть імя: ')
+    phone = None
+    for row in DATA[1:]:
+        if row[0] == name:
+            phone = row[2]
             break
-        else:
-            print("Щось не так. Спробуйте знов.")
+    if phone is not None:
+        print(f'Телефонний номер {name} це {phone}')
+    else:
+        print(f'{name} хто це? в нас немає таких')
 
-if __name__ == "__main__":
-    main()
+def show():
+    DATA = check_file()
+    for row in DATA:
+        print(', '.join(row))
+
+def delete():
+    DATA = check_file()
+    index = int(input('Введіть індекс: '))
+    DATA.pop(index)
+    with open('data.txt', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(DATA)
+    print('Данні видалені!')
+
+commands = {
+    'add': add,
+    'change': change,
+    'search': search,
+    'show': show,
+    'delete': delete,
+}
+print('Раді вас вітати в нашому боті!')
+print('Ви можете використовувати наступні команди:')
+print('add - додати нові данні')
+print('change - щоб змінити існуючі дані')
+print('search - щоб знайти номер телефона за імям')
+print('show - показати усі данні')
+print('delete - щоб видалити існуючі данні')
+print('exit - щоб вийти з програми')
+
+while True:
+    command = input('Введіть команду: ')
+    if command in commands:
+        commands[command]()
+    elif command == 'exit':
+        print('Бувайте здорові!')
+        break
+    else:
+        print('Щось не так')
